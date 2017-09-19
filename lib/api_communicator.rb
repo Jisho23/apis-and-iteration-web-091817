b388ac0530ba1ecf2577ad0ae7554540d2b1fc71
github username <github email address>
2017-09-19 14:26:20 -0400

    Fixed conflict with nil values.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -5,34 +5,24 @@ require 'pry'
 def get_character_movies_from_api(character)
   #make the web request
   character_hash = ruby_hasher('http://www.swapi.co/api/people/')
-
   hash_character_movies = {}
   while character_hash
     character_hash["results"].each do |current_character|
       if current_character["name"].downcase == character.downcase
         current_character["films"].each do |api_link|
-          current_movie_json = RestClient.get(api_link)
-          current_movie_hash = JSON.parse(current_movie_json)
+          current_movie_hash = ruby_hasher(api_link)
           hash_character_movies[current_movie_hash["episode_id"]] = current_movie_hash["title"]
         end
         return hash_character_movies
       end
     end
     if character_hash["next"] == nil
-      break
+      puts "Please put in a real character."
+      return nil
     end
     character_hash = ruby_hasher(character_hash["next"])
   end
 
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
 end
 
 def ruby_hasher(api_link)
@@ -40,11 +30,14 @@ def ruby_hasher(api_link)
   character_hash = JSON.parse(all_characters)
 end
 
-def parse_character_movies(films_hash)
+def parse_character_movies(films_hash=nil)
   # some iteration magic and puts out the movies in a nice list
-  films_hash.sort_by {|num, name| num}.each do |num, name|
-    puts "#{num}. #{name}"
+  if films_hash != nil
+    films_hash.sort_by {|num, name| num}.each do |num, name|
+      puts "#{num}. #{name}"
+    end
   end
+
 end
 
 def show_character_movies(character)
