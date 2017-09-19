1ecc5001708563e8e0a53643fe2bdd0792eed9c2
github username <github email address>
2017-09-19 14:05:36 -0400

    Code fixed to work with more than page 1

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,24 +4,26 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
+  character_hash = ruby_hasher('http://www.swapi.co/api/people/')
+
   hash_character_movies = {}
-  character_hash["results"].each do |current_character|
-
-    if current_character["name"].downcase == character.downcase
-      current_character["films"].each do |api_link|
-        current_movie_json = RestClient.get(api_link)
-        current_movie_hash = JSON.parse(current_movie_json)
-        hash_character_movies[current_movie_hash["episode_id"]] = current_movie_hash["title"]
-
+  while character_hash
+    character_hash["results"].each do |current_character|
+      if current_character["name"].downcase == character.downcase
+        current_character["films"].each do |api_link|
+          current_movie_json = RestClient.get(api_link)
+          current_movie_hash = JSON.parse(current_movie_json)
+          hash_character_movies[current_movie_hash["episode_id"]] = current_movie_hash["title"]
+        end
+        return hash_character_movies
       end
-
     end
-
+    if character_hash["next"] == nil
+      break
+    end
+    character_hash = ruby_hasher(character_hash["next"])
   end
 
-  hash_character_movies
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -31,6 +33,11 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+end
+
+def ruby_hasher(api_link)
+  all_characters = RestClient.get(api_link)
+  character_hash = JSON.parse(all_characters)
 end
 
 def parse_character_movies(films_hash)
