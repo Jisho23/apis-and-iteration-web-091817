03b71b1e48e472aaea41730220daadb6a55d5fbc
github username <github email address>
2017-09-19 14:43:33 -0400

    Added exit loop

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,11 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+while
+  character = get_character_from_user
+  if character.downcase == "exit"
+    puts "Goodbye!"
+    exit
+  end
+  show_character_movies(character)
+end
